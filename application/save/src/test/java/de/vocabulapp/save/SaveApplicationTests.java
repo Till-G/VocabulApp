21de77377ed46a80c1d93d92ef43137a6356dd72
Till-G <t.greiffertmail.com>
2021-06-08 14:32:32 +0200

    added SAVE microservice

---
+++ application/save/src/test/java/de/vocabulapp/save/SaveApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vocabulapp.save;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

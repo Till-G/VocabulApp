3eb4ae1967ca3d60d4fe9f844b4d6382b76a1d94
iremB <irem.belik@student.reutlingen.university.de>
2021-06-24 15:36:22 +0200

    added SignUp and LogIn files; method to save a card

--- frontend_files/script/main.js
+++ frontend_files/script/main.js
@@ -1,3 +1,20 @@
-function save(){
-    
+function addCard(){
+    var card = {
+		name: $('#name').val(),
+		german: $('german').val(),
+		english: $('english').val(),
+		turkish: $('turkish').val(),
+		spanish: $('spanish').val(),
+	};
+	$.ajax({
+		url: 'http://localhost:8989/card/add',
+		type: 'POST',
+		data: JSON.springify(card),
+		success: function(data){
+			alert('Card added successfully')
+		},
+		error: function(){
+			alert('Card was not added');
+		}
+	});
 }

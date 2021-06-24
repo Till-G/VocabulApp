34c67b7105f6b799de996f2fea17e455b0a367d6
iremB <irem.belik@student.reutlingen.university.de>
2021-06-24 22:53:42 +0200

    added table for all cards without functionality

--- frontend_files/script/main.js
+++ frontend_files/script/main.js
@@ -18,3 +18,35 @@ function addCard(){
 		}
 	});
 }
+
+var myArray = []
+
+
+	$.ajax({
+		method:'GET',
+		url:'http://localhost:8989/card/all',
+		success:function(response){
+			myArray = response.data
+			buildTable(myArray)
+			console.log(myArray)
+		}
+	})
+
+
+
+	function buildTable(data){
+		var table = document.getElementById('myTable')
+
+		for (var i = 0; i < data.length; i++){
+			var row = `<tr>
+							<td>${data[i].set}</td>
+							<td>${data[i].german}</td>
+							<td>${data[i].english}</td>
+							<td>${data[i].turkish}</td>
+							<td>${data[i].spanish}</td>
+					  </tr>`
+			table.innerHTML += row
+
+
+		}
+	}

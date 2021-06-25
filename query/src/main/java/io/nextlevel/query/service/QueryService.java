3f85be8a24f2886dccca268698844aff108ebc32
Okan Kaya <kaya.okan@live.de>
2021-06-25 19:35:59 +0200

    thrown out trash

--- query/src/main/java/io/nextlevel/query/service/QueryService.java
+++
@@ -1,8 +0,0 @@
-package io.nextlevel.query.service;
-
-public class QueryService {
-    private String query = "This is the query-Site";
-
-    public String getQuery(){return this.query;}
-    public void setQuery(String query){this.query = query;}
-}
